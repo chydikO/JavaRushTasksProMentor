b733074c1ba51d5948d3b4267499420f3b105a08
MbPro13_chudikO <oleg.chudnovskiy.gmail.com>
2023-05-22 22:48:12 +0300

    ua.javarush.task.pro.task04.task0401

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,18 @@
+package ua.javarush.task.pro.task04.task0401;
+
+/* 
+Я ніколи не працюватиму за дрібняки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String quote = "Я ніколи не працюватиму за дрібняки. Аміго";
+        //напишіть тут ваш код
+        int counter = 0;
+        while (counter < 100) {
+            System.out.println(quote);
+            counter++;
+        }
+
+    }
+}
